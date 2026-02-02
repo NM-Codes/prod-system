1c43c3b7ff31ec52822337ab14c1411f17946720
sahar <saharr.shirazii@gmail.com>
2026-02-02 13:55:56 +0100

    created files

---
+++ my-react-project/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Tasks from  "./pages/Tasks"
+import Timer from "./pages/Timer"
+import HistoryPage from "./pages/History"
+
+//Mappar sidnamn till komponenter för dynamisk rendering
+const pages = {
+  Home: <Home />,
+  Dashboard: <Dashboard />,
+  Tasks: <Tasks />,
+  Timer: <Timer />,
+  History: <HistoryPage />
+}
+
+function App() {
+  const [ activePage, setActivePage ] = useState("Home")
+
+  return(
+    <div className='body-container'>
+      {/* Header tar emot funktionen för att byta sida samt nuvarande status */}
+      <Header changePage={setActivePage} activePage={activePage}/>
+      
+      <main>
+        {/* Renderar den sida som matchar nuvarande state */}
+        {pages[activePage]}
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ my-react-project/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return(
+    <div className="main-container">
+
+    </div>
+  )
+}
+
+export default Dashboard
