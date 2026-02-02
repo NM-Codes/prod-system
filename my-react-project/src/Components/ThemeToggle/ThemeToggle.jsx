7cdddc9f7191c8d8534b348dc419b9f0ed683c91
sahar <saharr.shirazii@gmail.com>
2026-02-02 15:52:49 +0100

    images

---
+++ my-react-project/src/Components/ThemeToggle/ThemeToggle.jsx
@@ -0,0 +1,10 @@
+import { ThemeContext } from '../Contexts/ThemeContext.jsx'
+
+export default ThemeToggle (){
+    const themeToggle = useContext(ThemeContext);
+
+    return(
+        <button>
+        </button>
+    );
+}
