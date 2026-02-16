365f86dd0ff61f44d89e67fb8df59bd0c9159adc
nm <nm.codes@hotmail.com>
2026-02-16 16:35:09 +0100

    Add code

---
+++ my-react-project/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import "./Dashboard.css";
+
+function Dashboard() {
+
+    return(
+        // NAVBAR SKA IN HÄR STICKY POSITION*/}
+
+        <div className="dashboard">
+            <header className="dashboard-header">
+                <h1>Dashboard</h1>
+                <p>Snabb översikt över din produktivitet</p>
+            </header>
+        </div>
+    )
+}
