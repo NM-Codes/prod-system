30a2c085582f179f9bc64d74240f811b532cf9b1
johannasandstrom8183@gmail.com <johannasandstrom8183@gmail.com>
2026-02-25 11:07:22 +0100

    feat: css styling for History Page

--- my-react-project/src/Components/History/History.jsx
+++ my-react-project/src/Components/History/History.jsx
@@ -1,16 +1,37 @@
 import HistoryList from "./HistoryList";
+import HistoryFilter from "./HistoryFilter"; 
+import './History.css';
+import Card from "../Cards/Cards";
 
 function History({ sessions, onEdit, onDelete }) {
+  const hasNoSessions = !sessions || sessions.length === 0;
   return (
     <section className="history-page">
-      <h1>History</h1>
+      <h1>Historik</h1>
+      <p>Se och hantera dina tidigare sessioner</p>
 
+      
+      <HistoryFilter />
+
+{hasNoSessions ? (
+  <div className="wrapper-empty-history-content">
+  <Card>
+    <div className="empty-history-content">
+      <p>Inga sessioner ännu.</p>
+      <span>Starta din första timer!</span>
+    </div>
+  </Card>
+  </div>
+) : (
+
+      
       <HistoryList
         sessions={sessions}
         onEdit={onEdit}
         onDelete={onDelete}
       />
-    </section>
+      )}
+      </section>
   );
 }
 
